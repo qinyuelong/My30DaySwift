36a6685136130df2b93887f90ebaed7f1a926a92
qinyuelong <qinyuelong.cn@gmail.com>
2018-12-03 10:20:08 +0800

    加入JSExport 实例工程

---
+++ JSCoreDemo/JSCoreDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSCoreDemo
+//
+//  Created by LinkE on 2018/11/28.
+//  Copyright © 2018年 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSCoreDemo/JSCoreDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JSCoreDemo
+//
+//  Created by LinkE on 2018/11/28.
+//  Copyright © 2018年 dev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
